d72e948f587dbadff98bce9f34db26e578f0a5db
samshatan <sameermaurya97044@gmail.com>
2025-05-25 15:10:56 +0530

    opps

--- Javascript/oops/javascript-oops.js
+++ Javascript/oops/javascript-oops.js
@@ -26,40 +26,106 @@
 
 // Getter and Setter
 
-class Employee{
-  constructor(name, salary, age, isManager, dateOfJoining){
-    this.name = name;
-    this.salary = salary;
-    this.age = age;
-    this.isManager = isManager;
-    this.dateOfJoining = dateOfJoining;
-  }
+// class Employee{
+//   static companyName = "Company Name";
+//   constructor(name, salary, age, isManager, dateOfJoining){
+//     this.name = name;
+//     this.salary = salary;
+//     this.age = age;
+//     this.isManager = isManager;
+//     this.dateOfJoining = dateOfJoining;
+//   }
 
-  setName(name){
-    this.name = name;
-  }
+//   setName(name){
+//     this.name = name;
+//   }
 
-  getName(){
-    return this.name;
-  }
+//   getName(){
+//     return this.name;
+//   }
 
-  setSalary(salary){
-    this.salary = salary;
-  }
+//   setSalary(salary){
+//     this.salary = salary;
+//   }
 
-  getSalary(){
-    return this.salary;
-  }
+//   getSalary(){
+//     return this.salary;
+//   }
 
-  static calculateNewSalarayAfterBonus(employeeSalary, bonusPercentage){
-    return employeeSalary + (employeeSalary * bonusPercentage)/100
-  }
+//   static calculateNewSalarayAfterBonus(employeeSalary, bonusPercentage){
+//     return employeeSalary + (employeeSalary * bonusPercentage)/100
+//   }
+// }
+
+// let employee1 = new Employee("Sameer",3000000,20,false,'January');
+// // employee1.setName("Sha");
+// // console.log(employee1);
+// let newSalary = Employee.calculateNewSalarayAfterBonus(employee1.getSalary(), 10);
+// console.log(newSalary);
+// employee1.setSalary(newSalary);
+// console.log(employee1.getSalary());
+// console.log(Employee.companyName);
+
+
+// Inheritance
+
+// class car{
+//   static wheel = 4;
+//   constructor(color, wheelsColor, brand, engine){
+//     this.color = color;
+//     this.wheelsColor = wheelsColor;
+//     this.brand = brand;
+//     this.engine = engine;
+//   }
+// }
+
+// class ElectricCar extends car{
+//   constructor(color, wheelsColor, brand, engine, batteryBackup, chargingTime){
+//     super(color,wheelsColor,brand,engine);
+//     this.batteryBackup = batteryBackup;
+//     this.chargingTime = chargingTime;
+//   }
+// }
+
+// class PetrolCar extends car{
+//   constructor(color, wheelsColor, brand, engine, milage){
+//     super(color,wheelsColor,brand,engine);
+//     this.milage = milage;
+//   }
+// }
+
+// let electriccar = new ElectricCar("white", "black", "Audi", 3000, "400km", "4 Hour");
+// let electriccar2 = new ElectricCar("white", "black", "BMW", 2000, "500km", "4 Hour");
+// console.log(electriccar);
+// console.log(electriccar2);
+
+// class car{
+//   constructor(){
+//     console.log("Hello i m inside car");
+//   }
+// }
+
+// class electricCar extends car{
+//   constructor(){
+//     super();
+//     console.log("Hello i m inside electric car");
+//   }
+// }
+
+// let electric = new electricCar();
+
+// Javascript Object Notation (json)
+/* json
+{
+  "Name" : "Sameer",
+  "age" : 30,
+  "salary" : 2000.0,
+  "companyName" : "ABC"
 }
+*/
+// let obj_json = '{ "Name" : "Sameer", "age" : 30, "salary" : 2000.0, "companyName" : "ABC"}'
+// console.log(obj_json); // stringified version of json
 
-let employee1 = new Employee("Sameer",3000000,20,false,'January');
-// employee1.setName("Sha");
-// console.log(employee1);
-let newSalary = Employee.calculateNewSalarayAfterBonus(employee1.getSalary(), 10);
-console.log(newSalary);
-employee1.setSalary(newSalary);
-console.log(employee1.getSalary());
+// let object = JSON.parse(obj_json);
+// console.log(object);
+
--- newww.java
+++ newww.java
@@ -1,37 +1,56 @@
 import java.util.HashMap;
 
 public class newww{
+    private isPrime(int n){
+        if (n<=1){
+            return false;
+        }
+        if (n<=3){
+            return true;
+        }
+        if(n%2==0 || num%3==0){
+            return false;
+        }
+        int i=5;
+        while(i*i<=n){
+            if(n%i==0 || n%(i+2)==0){
+                return false;
+            i+=6;
+        return true;
+            }
+        }
+    }
     public static void main(String[] args) {
-        String s1 = "this apple is sweet";
-        String s2 = "this apple is sour";
-        String[] arr1 = s1.split(" ");
-        String[] arr2 = s2.split(" ");
-        HashMap<String, Integer> count=new HashMap<>();
-        for(int i=0; i<arr1.length; i++)
-        {
-            count.put(arr1[i],count.getOrDefault(arr1[i],0)+1);
-        }
-        for(int i=0; i<arr2.length; i++)
-        {
-            count.put(arr2[i],count.getOrDefault(arr2[i],0)+1);
-        }
-        System.out.println(count);
-        int counti = 0;
-        for(String s : count.keySet()){
-            if(count.get(s)==1){
-                counti++;
-            }
-        }
-        String ans[] = new String[counti];
-        int p = 0;
-        for(String s : count.keySet()){
-            if(count.get(s)==1){
-                ans[p] = s;
-                p++;
-            }
-        }
-        for(int i=0;i<counti;i++){
-            System.out.println(ans[i]);
-        }
+        // String s1 = "this apple is sweet";
+        // String s2 = "this apple is sour";
+        // String[] arr1 = s1.split(" ");
+        // String[] arr2 = s2.split(" ");
+        // HashMap<String, Integer> count=new HashMap<>();
+        // for(int i=0; i<arr1.length; i++)
+        // {
+        //     count.put(arr1[i],count.getOrDefault(arr1[i],0)+1);
+        // }
+        // for(int i=0; i<arr2.length; i++)
+        // {
+        //     count.put(arr2[i],count.getOrDefault(arr2[i],0)+1);
+        // }
+        // System.out.println(count);
+        // int counti = 0;
+        // for(String s : count.keySet()){
+        //     if(count.get(s)==1){
+        //         counti++;
+        //     }
+        // }
+        // String ans[] = new String[counti];
+        // int p = 0;
+        // for(String s : count.keySet()){
+        //     if(count.get(s)==1){
+        //         ans[p] = s;
+        //         p++;
+        //     }
+        // }
+        // for(int i=0;i<counti;i++){
+        //     System.out.println(ans[i]);
+        // }
     }
 }
